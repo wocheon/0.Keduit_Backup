f62ce204e6af76a3d76346ec43c45831b7c1212d
wocheon <82438284+wocheon@users.noreply.github.com>
2023-07-14 02:05:12 +0900

    Delete github_backup directory

--- github_backup/KVM_php-main/include/mariadbcon.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$con = mysqli_connect("172.17.0.3", "root", "diak1351!","testdb") or die ("Mysql connection fail !!!");
-echo "mariadb con success!";
-?>
--- github_backup/KVM_php-main/ins2.sh
+++
@@ -1,6 +0,0 @@
-#libvert를 이용하여 이미지 설치 및 인스턴스  
-
-virt-builder cirros-0.3.5 --size 5G -o /root/${1}.img
-
-sudo virt-install --name ${1} --vcpus ${2} --ram ${3} --noautoconsole --graphics none  --disk path=/root/${1}.img --import
-
